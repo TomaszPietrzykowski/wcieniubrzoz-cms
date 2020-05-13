2d32ab5981cafce8d3782e8a81b78ea40a3d9174
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-05-13 13:12:05 +0200

    Tips section setup

--- src/components/pages/Tips.js
+++ src/components/pages/Tips.js
@@ -1,12 +1,60 @@
-import React from "react"
+import React, { useState, useEffect, Fragment } from "react"
+
+import SectionHeader from "../ui/SectionHeader"
+import HelpBtn from "../ui/HelpBtn"
+import DisplayTipList from "../legends/DisplayTipList"
+import EditTip from "../legends/EditTip"
+import Loader from "../ui/Loader"
 
 const Tips = () => {
+  const [tips, setTips] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [editedTip, setEditedTip] = useState({})
+  const [activeTab, setActiveTab] = useState("list")
+
+  useEffect(() => {
+    getTips()
+  }, [])
+
+  const getTips = async () => {
+    window.scroll(0, 0)
+    setLoading(true)
+    try {
+      const res = await fetch("https://barracudadev.com/api/v1/legends")
+      const data = await res.json()
+      const downloaded = data.data
+      setTips(downloaded)
+
+      setLoading(false)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div>
-      <br />
-      <br />
-      <h1 style={{ textAlign: "center", color: "#777" }}>Tips</h1>
-      <p style={{ minHeight: "60vh" }}></p>
+      <SectionHeader
+        title={activeTab === "edit" ? "Edytuj poradę" : "Porady Ogrodnika"}
+      />
+      {loading ? (
+        <Loader />
+      ) : activeTab === "edit" ? (
+        <EditTip
+          tip={editedTip}
+          setEditedTip={setEditedTip}
+          setActiveTab={setActiveTab}
+          getTips={getTips}
+        />
+      ) : (
+        <Fragment>
+          <DisplayTipList
+            tips={tips}
+            setEditedTip={setEditedTip}
+            setActiveTab={setActiveTab}
+          />
+          <HelpBtn />
+        </Fragment>
+      )}
     </div>
   )
 }
